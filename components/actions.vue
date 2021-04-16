5c2a941bb3701b3e040e6bccefd85fe826228fca
carlos <carlos@creativosdigitales.co>
2021-04-16 10:53:17 -0500

    primer  commit

---
+++ components/actions.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- <v-card v-show="itemsFiltered.length != 0 || search == ''"> -->
+  <v-card>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-row>
+        <v-col>
+          <Button
+            :name="'All'"
+            :block="true"
+            @clicked="changeCategory(true)"
+            :color="getColor(category, '#F22539', '#BFBFBF')"
+            :icon="'mdi-format-list-bulleted'"
+          />
+        </v-col>
+        <v-col>
+          <Button
+            :block="true"
+            :name="'Favorites'"
+            @clicked="changeCategory(false)"
+            :color="getColor(!category, '#F22539', '#BFBFBF')"
+            :icon="'mdi-star'"
+          />
+        </v-col>
+      </v-row>
+
+      <v-spacer> </v-spacer>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { utils } from '@/mixins/utils'
+import Button from '@/components/utils/Button'
+export default {
+  mixins: [utils],
+  props: ['category'],
+  components: {
+    Button,
+  },
+  methods: {
+    changeCategory(cat) {
+      this.$emit('change-category', cat)
+    },
+  },
+}
+</script>
---
+++ components/utils/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-btn
+      :color="color"
+      class="button no-text-transform"
+      @click="clicked()"
+      dark
+      :block="block"
+    >
+      <v-icon v-if="icon" class="icon"> {{ icon }} </v-icon>
+      {{ name }}
+    </v-btn>
+  </div>
+</template>
+<script>
+export default {
+  props: ['name', 'icon', 'color', 'block'],
+  methods: {
+    clicked() {
+      this.$emit('clicked')
+    },
+  },
+}
+</script>
+
+
+
+
+<style>
+.button {
+  background-color: #f22539;
+  border-radius: 60px;
+}
+.icon {
+  margin: 0 5px 0 5px;
+}
+</style>
